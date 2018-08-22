4a99fd419a4415758a0cc4fb521ac308da48dc52
Robert Reed <robert.mc.reed@gmail.com>
2018-08-22 10:00:15 -0700

    adding main

---
+++ main.js
@@ -0,0 +1,34 @@
+const arDrone = require('ar-drone');
+const client = arDrone.createClient();
+
+try {
+  client.takeoff();
+  
+  client.on('navdata', data => {
+    console.log('[INFO] Navdata');
+    console.log(data);
+  });
+  
+  const pngStream = client.getPngStream();
+  pngStream.on('data', data => {
+    console.log('[INFO] PNG Stream');  
+    console.log(data);
+  });
+  
+  client
+    .after(5000, function() {
+      this.clockwise(0.5);
+    })
+    .after(3000, function() {
+      this.animate('flipLeft', 15);
+    })
+    .after(1000, function() {
+      this.stop();
+      this.land();
+    });
+} catch (e) {
+    console.log('[ERROR]')
+    console.log(e);
+    client.stop();
+    client.land();
+}
